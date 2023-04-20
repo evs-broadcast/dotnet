6279e80202c61212b71424f7093af15410279726
MIGNOLET, Pierre <p.mignolet@evs.com>
2023-04-20 13:41:54 +0200

    Add test on tokenizer

--- Structurizr.Dsl.Test/IParserTests.cs
+++ Structurizr.Dsl.Test/IParserTests.cs
@@ -15,5 +15,14 @@ namespace Structurizr.DslReader.Test
       Assert.NotNull(parser);
       Assert.IsType<CommentParser>(parser);
     }
+
+    [Fact]
+    public void Tokenizer_HandleOpenBracketWithoutSpace()
+    {
+      const string line = "sub_search = softwareSystem \"via-search\"{";
+      var tokens = Tokenizer.Tokenize(line);
+      Assert.Equal("{", tokens.Last());
+      Assert.Equal(5,tokens.Count());
+    }
   }
 }
