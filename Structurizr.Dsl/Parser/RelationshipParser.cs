c3f36df26606f250179d323c151b0718ae69ecc0
MIGNOLET, Pierre <p.mignolet@evs.com>
2023-04-26 18:30:31 +0200

    add naming convention checks

--- Structurizr.Dsl/Parser/RelationshipParser.cs
+++ Structurizr.Dsl/Parser/RelationshipParser.cs
@@ -1,3 +1,4 @@
+using System.Data;
 using Microsoft.Extensions.Logging;
 
 namespace Structurizr.DslReader.Parser
@@ -5,6 +6,7 @@ namespace Structurizr.DslReader.Parser
   public sealed class RelationshipParser : IParser
   {
     private const string RELATION = "->";
+    private static readonly string[] TECH = { "https", "WS", "command", "event", "grpc","wf" };
     public bool Accept(string line, ParsingContext context)
     {
       return line.Contains($"{RELATION} ", StringComparison.InvariantCultureIgnoreCase);
@@ -14,13 +16,15 @@ namespace Structurizr.DslReader.Parser
     {
       var tokens = line.Split(' ');
 
+      Relationship relationship;
+
       if (tokens.GetValueAtOrDefault(0) == RELATION) /*-> <identifier> [description] [technology] [tags]*/
       {
         var destinationId = tokens.GetValueAtOrDefault(1);
         var destination = contextualWorkspace.Workspace.Model.GetElement(destinationId);
         if (destination == null) throw new Exception($"Unable to find element [{destinationId}]");
 
-        contextualWorkspace.Workspace.Model.AddRelationship(contextualWorkspace.Context.Element, destination, tokens.GetValueAtOrDefault(2), tokens.GetValueAtOrDefault(3), null, tokens.GetTagsAt(4));
+        relationship = contextualWorkspace.Workspace.Model.AddRelationship(contextualWorkspace.Context.Element, destination, tokens.GetValueAtOrDefault(2), tokens.GetValueAtOrDefault(3), null, tokens.GetTagsAt(4));
       }
       else if (tokens.GetValueAtOrDefault(1) == RELATION) /*<identifier> -> <identifier> [description] [technology] [tags] */
       {
@@ -33,10 +37,17 @@ namespace Structurizr.DslReader.Parser
         var destination = contextualWorkspace.Workspace.Model.GetElement(destinationId);
         if (destination == null) throw new Exception($"Unable to find element [{destinationId}]");
 
-        contextualWorkspace.Workspace.Model.AddRelationship(source, destination, tokens.GetValueAtOrDefault(3), tokens.GetValueAtOrDefault(4), null, tokens.GetTagsAt(5));
+        relationship = contextualWorkspace.Workspace.Model.AddRelationship(source, destination, tokens.GetValueAtOrDefault(3), tokens.GetValueAtOrDefault(4), null, tokens.GetTagsAt(5));
       }
       else
+      {
         throw new Exception($"unable to parse [{line}]");
+      }
+
+      if (!TECH.Any(t => t == relationship.Technology))
+        contextualWorkspace.AddNamingConventionError(directoryInfo.Name, lineNumber, $"Relationship technology MUST be {string.Join(" or ", TECH)}");
+
+      relationship.AddTags(relationship.Technology);
 
       return ValueTask.FromResult(contextualWorkspace);
     }
