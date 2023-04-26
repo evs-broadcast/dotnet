1b9fd0c2d136bc96b7d56533153dd1a0bcd7ad69
MIGNOLET, Pierre <p.mignolet@evs.com>
2023-04-26 19:45:12 +0200

    add relationship

--- Structurizr.Dsl/Parser/RelationshipParser.cs
+++ Structurizr.Dsl/Parser/RelationshipParser.cs
@@ -6,7 +6,7 @@ namespace Structurizr.DslReader.Parser
   public sealed class RelationshipParser : IParser
   {
     private const string RELATION = "->";
-    private static readonly string[] TECH = { "https", "WS", "command", "event", "querycommand", "queryevent", "grpc","wf","db" };
+    private static readonly string[] TECH = { "http","https", "WS", "command", "event", "querycommand", "queryevent", "grpc","wf","db", "ZMQ" };
     public bool Accept(string line, ParsingContext context)
     {
       return line.Contains($"{RELATION} ", StringComparison.InvariantCultureIgnoreCase);
