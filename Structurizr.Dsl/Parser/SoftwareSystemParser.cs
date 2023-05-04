361d281b9f11c85c00b6b0987419a43bac00abc7
MIGNOLET, Pierre <p.mignolet@evs.com>
2023-05-04 08:29:03 +0200

    Add tags for software systems

--- Structurizr.Dsl/Parser/SoftwareSystemParser.cs
+++ Structurizr.Dsl/Parser/SoftwareSystemParser.cs
@@ -21,16 +21,21 @@ namespace Structurizr.DslReader.Parser;
     if (string.Compare(tokens.GetValueAtOrDefault(0), SOFTWARE_SYSTEM, true) == 0) // softwareSystem = {name} {description}
     {
       softwareSystem = contextualWorkspace.Workspace.Model.AddSoftwareSystem(tokens.GetValueAtOrDefault(2), tokens.GetValueAtOrDefault(3));
+      softwareSystem.AddTags(tokens.GetTagsAt(4));
     }
     else
     {
       if (tokens.GetValueAtOrDefault(1) == "=") //{id} = SoftwareSystem {name}
       {
         softwareSystem = contextualWorkspace.Workspace.Model.AddSoftwareSystem(tokens.GetValueAtOrDefault(0), Location.Unspecified, tokens.GetValueAtOrDefault(3), tokens.GetValueAtOrDefault(4));
+        softwareSystem.AddTags(tokens.GetTagsAt(5));
       }
       else
         throw new ParsingException(directoryInfo.Name, lineNumber, $"Unable to parse {SOFTWARE_SYSTEM}");
     }
+
+    if (softwareSystem == null)
+      throw new Exception($"Dupplicate {SOFTWARE_SYSTEM} id {tokens.GetValueAtOrDefault(0)} at line {line} in file {directoryInfo.Name}/workspace.dsl");
 
     AssetNamingConvention(contextualWorkspace, directoryInfo, softwareSystem, lineNumber);
 
