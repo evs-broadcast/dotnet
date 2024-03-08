0e5a0401070caba11afd57b45802a26e5281ee2d
MIGNOLET, Pierre <p.mignolet@evs.com>
2024-03-08 17:52:38 +0100

    support extend container and componnent

--- Structurizr.Dsl/Parser/RefParser.cs
+++ Structurizr.Dsl/Parser/RefParser.cs
@@ -5,22 +5,36 @@ namespace Structurizr.DslReader.Parser
   public sealed class RefParser : IParser
   {
     private const string REF = "!ref";
+    private const string EXTEND = "!extend";
     public bool Accept(string line, ParsingContext context)
     {
-      return line.StartsWith($"{REF} ", StringComparison.InvariantCultureIgnoreCase);
+      return line.StartsWith($"{REF} ", StringComparison.InvariantCultureIgnoreCase) || line.StartsWith($"{EXTEND} ", StringComparison.InvariantCultureIgnoreCase);
     }
 
     public ValueTask<ContextualWorkspace> ParseAsync(string line, int lineNumber, ContextualWorkspace contextualWorkspace, DirectoryInfo directoryInfo, ILogger logger)
     {
       var tokens = Tokenizer.Tokenize(line);
 
-      if(contextualWorkspace.Context.Model != null)
+      if(contextualWorkspace.Workspace.Model != null)
       {
-        var softwareSystemId = tokens.GetValueAtOrDefault(1);
-        var softwaresystem = contextualWorkspace.Context.Model.GetSoftwareSystemWithId(softwareSystemId);
-        if (softwaresystem == null)
-          throw new Exception($"Unknow Software system [{tokens.GetValueAtOrDefault(1)}]");
-        contextualWorkspace.Context.Set(softwaresystem);
+        var id = tokens.GetValueAtOrDefault(1);
+        var element = contextualWorkspace.Workspace.Model.GetElement(id);
+        if (element != null)
+        {
+          if(element is SoftwareSystem softwareSystem)
+          {
+            contextualWorkspace.Context.Set(softwareSystem);
+          }
+          else if(element is Container container)
+          {
+            contextualWorkspace.Context.Set(container);
+          }
+          else if (element is Component component)
+          {
+            contextualWorkspace.Context.Set(component);
+          }
+          else throw new Exception($"Unable to {REF} or {EXTEND}: {line}");
+        }
       }
       else
       {
